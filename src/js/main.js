a5a7e50d367ea2d5fe839c4e8e958b2a8d81bafd
Abolarin Olanrewaju Olabode <waju.abolarin@gmail.com>
2019-01-03 05:06:11 +0100

    initial commit

---
+++ src/js/main.js
@@ -0,0 +1,11 @@
+import '../index.html'
+import '../article.html'
+import '../scss/style.scss'
+/**Necessary for loadig styles and copying over html files */
+import hljs from  'highlight.js'
+import 'highlight.js/lib/languages/php'
+import 'highlight.js/lib/languages/javascript'
+import 'highlight.js/lib/languages/bash'
+import 'highlight.js/lib/languages/css'
+
+hljs.initHighlightingOnLoad()
